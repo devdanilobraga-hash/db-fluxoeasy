1bc4016b6edb6402dcf771bf1e6f4badad3cf75f
Danilo Braga <danilobragam@gmail.com>
2025-10-04 19:08:29 -0300

    commit

--- routes/planos.js
+++ routes/planos.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth'); 
 const {
   getPlanos,
   getPlanoById,
@@ -8,10 +9,12 @@ const {
   deletePlano
 } = require('../controllers/planosController');
 
+router.use(auth);
+
 router.get('/', getPlanos);
 router.get('/:id', getPlanoById);
-router.post('/', createPlano);
-router.put('/:id', updatePlano);
-router.delete('/:id', deletePlano);
+router.post('/', createPlano);      
+router.put('/:id', updatePlano);    
+router.delete('/:id', deletePlano); 
 
 module.exports = router;
